9bfcab0af85f3dca4e7167ed598b22d5b34c1e6a
Karl Shaffer <skmshaffer@gmail.com>
2020-02-18 15:34:59 -0600

    Scaffolds out Initial client and server codebase.

---
+++ src/library-server/src/main/java/school/raikes/library/libraryserver/LibraryServerApplication.java
@@ -0,0 +1,13 @@
+package school.raikes.library.libraryserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryServerApplication.class, args);
+	}
+
+}
---
+++ src/library-server/src/test/java/school/raikes/library/libraryserver/LibraryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package school.raikes.library.libraryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
