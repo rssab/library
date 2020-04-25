f33e7d61a0047515d9440d55d9e33f4691c98510
Karl Shaffer <skmshaffer@users.noreply.github.com>
2020-04-24 23:40:11 -0400

    Work Done on the Catalog Parser: - Adds various helpful dependencies - Builds out basic flow of CsvImportApplicationRunner - Starts work on CatalogCsvReader

---
+++ src/library-server/src/main/java/school/raikes/library/libraryserver/readers/CatalogCsvReader.java
@@ -0,0 +1,68 @@
+package school.raikes.library.libraryserver.readers;
+
+import com.google.common.base.Charsets;
+import com.google.common.collect.ImmutableSet;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
+import school.raikes.library.libraryserver.model.entity.*;
+
+import java.io.*;
+import java.util.*;
+
+public class CatalogCsvReader {
+
+    public static final Set<String> EMPTY_VALUES = ImmutableSet.of("", "-", "undefined");
+
+    /* KEYS */
+    public static final String BARCODE_KEY = "Barcode";
+    public static final String ISBN_KEY = "ISBN";
+    public static final String TITLE_KEY = "Title";
+    public static final String SUBTITLE_KEY = "Subtitle";
+    public static final String EDITION_KEY = "Edition";
+    public static final String AUTHORS_KEY = "Authors";
+    public static final String PUBLISH_DATE_KEY = "Publish Date";
+    public static final String ACQUISITION_DATE_KEY = "Acquisition Date";
+    public static final String SHELF_KEY = "Shelf";
+    public static final String MANUAL_KEY = "Manual";
+
+
+    Map<Integer, Shelf> shelfMap;
+    Map<String, Type> typeMap;
+    Map<String, Tag> tagMap;
+    Map<String, Author> authorMap;
+    Map<Book, List<Copy>> bookCopyMap;
+    Map<String, Book> isbnBookMap;
+
+    public CatalogCsvReader() {
+        this.shelfMap = new HashMap<>();
+        this.typeMap = new HashMap<>();
+        this.tagMap = new HashMap<>();
+        this.authorMap = new HashMap<>();
+        this.bookCopyMap = new HashMap<>();
+        this.isbnBookMap = new HashMap<>();
+    }
+
+    /**
+     * Reads a Catalog from a CSV file
+     * @param file the CSV file
+     * @throws IOException if any errors occur while reading the file or parsing the CSV.
+     */
+    public void read(File file) throws IOException {
+        read(new FileInputStream(file));
+    }
+
+    /**
+     * Reads a Catalog from a CSV input.
+     * @param inputStream An {@link InputStream} for a CSV file.
+     * @throws IOException if any errors occur while parsing the CSV.
+     */
+    public void read(InputStream inputStream) throws IOException {
+        CSVParser parser = CSVParser.parse(inputStream, Charsets.UTF_8, CSVFormat.EXCEL.withFirstRecordAsHeader());
+
+        for (CSVRecord row : parser) {
+
+        }
+    }
+
+}
--- src/library-server/src/main/java/school/raikes/library/libraryserver/startup/CsvImportApplicationRunner.java
+++ src/library-server/src/main/java/school/raikes/library/libraryserver/startup/CsvImportApplicationRunner.java
@@ -5,19 +5,38 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
+import school.raikes.library.libraryserver.readers.CatalogCsvReader;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.Arrays;
 
 @Component
 @Profile("dev")
 @Slf4j
 public class CsvImportApplicationRunner implements ApplicationRunner {
+
+    private static final String CSV_FILE_PARAMETER = "importcsvfile";
+
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        log.error("This is running as expected.");
-        log.error(Arrays.toString(args.getSourceArgs()));
-        for (String str : args.getOptionNames()) {
-            log.warn(str);
+        log.info("Checking for CSV catalog to import");
+        if (args.containsOption(CSV_FILE_PARAMETER)) {
+            String filepath = args.getOptionValues(CSV_FILE_PARAMETER).get(0);
+            log.info("Loading CSV file: {}", filepath);
+
+            File catalogFile = new File(filepath);
+            CatalogCsvReader catalogCsvReader = new CatalogCsvReader();
+
+            try {
+                catalogCsvReader.read(catalogFile);
+            } catch(IOException ioe) {
+                log.warn("Error occurred while attempting to import CSV file {}", catalogFile);
+                log.debug("Exception thrown: ", ioe);
+            }
+
+        } else {
+            log.info("No import required");
         }
     }
 }
